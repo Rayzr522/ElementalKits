adce97082ff43ff99902e4187347c5ea14bbf36e
Rayzr522 <rayzr522@users.noreply.github.com>
2016-09-01 12:46:16 -0400

    Added base class

---
+++ src/main/java/com/rayzr522/elementalkits/ElementalKits.java
@@ -0,0 +1,33 @@
+
+package com.rayzr522.elementalkits;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ElementalKits extends JavaPlugin {
+
+	private Logger logger;
+
+	@Override
+	public void onEnable() {
+
+		logger = getLogger();
+		
+		logger.info(versionString() + " enabled!");
+
+	}
+
+	public void onDisable() {
+		
+		logger.info(versionString() + " disabled!");
+		
+	}
+
+	public String versionString() {
+
+		return getName() + " v" + getDescription().getVersion();
+
+	}
+
+}
