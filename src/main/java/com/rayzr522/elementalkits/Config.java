ee43358f4e2a30f247cff73f5e99904be0c61f17
Rayzr522 <rayzr522@users.noreply.github.com>
2016-09-01 12:53:27 -0400

    Added buttloads of utility classes

---
+++ src/main/java/com/rayzr522/elementalkits/Config.java
@@ -0,0 +1,28 @@
+
+package com.rayzr522.elementalkits;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class Config {
+
+	// public static String PERM_HOME = "";
+	// public static String PERM_MORE = "";
+	// public static String PERM_OTHERS = "";
+	// public static String PERM_SETHOME = "";
+	// public static String PERM_DELHOME = "";
+	// public static String PERM_CUBEHOMES = "";
+
+	public static void load(ElementalKits plugin) {
+
+		FileConfiguration config = plugin.getConfig();
+
+		// PERM_HOME = config.getString("permissions.home");
+		// PERM_MORE = config.getString("permissions.more");
+		// PERM_OTHERS = config.getString("permissions.others");
+		// PERM_SETHOME = config.getString("permissions.sethome");
+		// PERM_DELHOME = config.getString("permissions.delhome");
+		// PERM_CUBEHOMES = config.getString("permissions.cubehomes");
+
+	}
+
+}
---
+++ src/main/java/com/rayzr522/elementalkits/utils/ConfigManager.java
@@ -0,0 +1,105 @@
+
+package com.rayzr522.elementalkits.utils;
+
+import java.io.File;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.rayzr522.elementalkits.ElementalKits;
+
+public class ConfigManager {
+
+	private ElementalKits plugin;
+
+	public ConfigManager(ElementalKits plugin) {
+
+		this.plugin = plugin;
+
+		if (!configYmlExists()) {
+
+			plugin.saveResource("config.yml", true);
+
+		}
+
+	}
+
+	public boolean configYmlExists() {
+
+		return getFile("config.yml").exists();
+
+	}
+
+	public YamlConfiguration getConfig(String path) {
+
+		return YamlConfiguration.loadConfiguration(getFile(path));
+
+	}
+
+	public YamlConfiguration getOrCreate(String path) {
+
+		File file = getFile(path);
+
+		if (!file.exists()) {
+			if (plugin.getResource(path) != null) {
+				plugin.saveResource(path, false);
+			} else {
+				try {
+					file.createNewFile();
+				} catch (Exception e) {
+					System.err.println("Failed to create file at '" + path + "'");
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return YamlConfiguration.loadConfiguration(file);
+
+	}
+
+	public File getFile(String path) {
+
+		return new File(plugin.getDataFolder() + File.separator + path);
+
+	}
+
+	public void backupConfig() {
+
+		if (!configYmlExists()) { return; }
+
+		File backupFile = getFile("config-backup.yml");
+
+		if (backupFile.exists()) {
+			backupFile.delete();
+		}
+
+		File configYml = getFile("config.yml");
+		configYml.renameTo(backupFile);
+
+		plugin.saveResource("config.yml", true);
+
+	}
+
+	public void saveConfig(String path, YamlConfiguration config) {
+
+		try {
+
+			File file = getFile(path);
+
+			if (!file.exists()) {
+
+				file.createNewFile();
+
+			}
+
+			config.save(file);
+
+		} catch (Exception e) {
+
+			System.err.println("Failed to save config file at '" + path + "'");
+			e.printStackTrace();
+
+		}
+
+	}
+
+}
